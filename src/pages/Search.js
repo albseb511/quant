a8f38c7721754b581469476f8bf2121a4a484b28
albseb511 <albert@wisense.in>
2019-08-06 09:38:41 +0530

    init

---
+++ src/pages/Search.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+import SearchBar from '../components/searchbar/SearchBar'
+
+class Search extends Component {
+    render(){
+        return (
+            <SearchBar/>
+        );
+    }
+}
+
+export default Search;
